b2d35aab1183defc916915a92d2312d560eae372
hamad-mf <hamadmf100@gmail.com>
2025-02-28 10:16:16 +0530

    l

--- lib/View/Home Screen/home_screen.dart
+++ lib/View/Home Screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+0
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,13 +10,13 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   TextEditingController nameController = TextEditingController();
-
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          print(nameController.text);
+          _formKey.currentState!.validate();
           setState(() {
             
           });
@@ -23,27 +24,49 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Icon(Icons.print),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: nameController,
-                readOnly: false,
-                decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: nameController,
+                  readOnly: false,
+                  decoration: InputDecoration(
                     label: Text("Name"),
-                    hintText: 'Enter your name',
-                    fillColor: Colors.grey,
-                    filled: true,
+                    hintText: "Enter",
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.red, width: 2)),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.black, width: 2)),
                     suffixIcon: Icon(Icons.person),
-                    prefixIcon: Icon(Icons.text_fields_outlined)),
+                    disabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.grey, width: 2)),
+                    errorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.yellow, width: 2)),
+                    focusedErrorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.blue, width: 2)),
+                    prefixIcon: Icon(Icons.search),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Enter a valid text";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),  
               ),
-            ),
-            Text(nameController.text),
-            
-          ],
+              Text(nameController.text),
+            ],
+          ),
         ),
       ),
     );
